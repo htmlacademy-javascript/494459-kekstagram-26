ac4e3875b999bd43a24363f29d8030f278b23077
Keks <keks@htmlacademy.ru>
2022-06-04 21:53:36 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,12 @@
+const getRandomNumber = (min, max) => {
+  if (min < 0 || max < 0) {
+    return 0;
+  }
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  const result = (Math.random() * (max + 1 - min) + min);
+  return Math.floor(result);
+};
+
+const checkMaxLength = (str, strLength) => str.length <= strLength;
