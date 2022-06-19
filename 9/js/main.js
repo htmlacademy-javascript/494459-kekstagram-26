5966603fec92daf1dd905356db111ef8c7e25a82
Keks <keks@htmlacademy.ru>
2022-06-19 14:47:51 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,5 @@
+import { showUsersPreview } from './preview.js';
+
+const SIMILAR_PHOTO_DATA_COUNT = 25;
+
+showUsersPreview(SIMILAR_PHOTO_DATA_COUNT);
---
+++ 9/js/mock/util.js
@@ -0,0 +1,32 @@
+import { Numbers } from '/js/mock/data.js';
+
+/**
+ * Функция getRandomNumber - генерирует случайное положительное число.
+ *
+ * @param {number} min - минимальное положительное число.
+ * @param {number} max - максимальное положительное число.
+ * @return {integer} - случайное целое число.
+ */
+
+const getRandomNumber = (min, max) => {
+  if (min < Numbers.ZERO || max < Numbers.ZERO) {
+    return Numbers.ZERO;
+  }
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  const result = (Math.random() * (max +  Numbers.MIN_RANDOM_NUMBER - min) + min);
+  return Math.floor(result);
+};
+
+/**
+ * Функция checkMaxLength - проверяет длинну строки.
+ *
+ * @param {string} str - строка для проверки.
+ * @param {number} strLength - число для проверки длинны строки.
+ * @return {boolean} - вовзращает true/false.
+ */
+
+const checkMaxLength = (str, strLength = Numbers.MAX_STR_LENGTH) => str.length <= strLength;
+
+export { getRandomNumber, checkMaxLength, Numbers };
