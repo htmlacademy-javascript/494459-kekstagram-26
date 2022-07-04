9ff4ce5cae12ce46672f9003582ebf16cf01f773
Aleksandr Daniel <tgaleksandr@gmail.com>
2022-07-04 14:19:34 +0300

    В процессе...

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import { renderThumbnails } from './preview.js';
 import { mockDataGenerate } from './mock/data.js';
-// import { uploadBtn } from './form.js';
+import { uploadBtn } from './form.js';
+import { scaleControl } from './filters.js';
 
+scaleControl();
 
 const COUNT_CARDS_FOR_RENDER = 25;
 const countMocksForRender = mockDataGenerate(COUNT_CARDS_FOR_RENDER);
